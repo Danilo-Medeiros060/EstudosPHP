f7b3802135222daf2293651f42bf11a857e95fa9
Danilo Medeiros <danilo.medeiros060@gmail.com>
2021-05-21 09:43:23 -0300

    1

--- index.php
+++ index.php
@@ -1,32 +1,37 @@
 <html>
- <head>
-  <title>Apostila PHP Progressivo</title>
- </head>
- <body>
-    <form action="" method="get">
-        Digite o número maximo: <input type="number" name="maxNumber" />
-        <input type="submit" name="submit" value="enviar" />
-    </form> 
- <?php
 
-$n = $_GET['maxNumber'];
- 
-for($i = 1; $i <= $n; $i++)
-{
-    $divisores = 0;
+    <head> 
 
-    for($j = $i; $j >= 1; $j--)
-    {
-        if (($i % $j) == 0) {
-            $divisores++;
-        }
-    }
-    
-    if ($divisores == 2)
-    {
-        echo $i . ', ';
-    }
-}
- ?>
- </body>
+         <meta charset="UTF-8">
+
+    </head>
+
+    <body>
+
+         <form action="" method="get">
+
+          Informe uma frase 
+
+          <input type="text" name="text"  />
+
+          <input type="submit" value="enviar" name="enviar"   / >
+
+         </form>
+
+
+
+    <?php
+
+          $n = $_GET['text'];
+
+           
+
+           $str = 'lmcrn';
+
+           echo strlen ($n);
+
+     ?>
+
+    </body>
+
 </html>
